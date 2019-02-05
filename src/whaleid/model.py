76d21354940af0a1bfe0a2856dbcaf2a0c0670f2
danjust <daju85@gmail.com>
2019-02-05 21:41:24 +0000

    prep for cloud deployment

--- src/whaleid/model.py
+++ src/whaleid/model.py
@@ -9,18 +9,15 @@ from whaleid import convnets
 
 class whalenet():
     """Model class"""
-    def __init__(self, iterator, learning_rate, model_name,
+    def __init__(self, iterator, learning_rate, model_name, margin,
                  convnet=convnets.WhaleCNN):
         self.iterator = iterator
         self.learning_rate = learning_rate
         self.model_name = model_name
         self.convnet = convnet
+        self.margin = margin
 
-        self.img1 = None
-        self.img2 = None
-        self.target_diff = None
-        self._results_diff = None
-        self._contrastive_loss = None
+        self._loss = None
         self._train_op = None
         self._summary_op = None
 
@@ -30,28 +27,26 @@ class whalenet():
 
 
     @property
-    def results_diff(self):
-        """Creates two identical CNNs and
-        returns difference of output features
+    def loss(self):
+        """Funtion creates two identical CNNs,
+        computes the difference of output features
+        and returns the contrastive loss
         """
-        if self._results_diff is None:
+        if self._loss is None:
+            next_batch = self.iterator.get_next()
+            img1, img2, target_diff = next_batch
 
-            features1 = self.convnet(self.img1, reuse=False)
-            features2 = self.convnet(self.img2, reuse=True)
-            self._results_diff = tf.sqrt(tf.reduce_mean(
+            features1 = self.convnet(img1, reuse=False)
+            features2 = self.convnet(img2, reuse=True)
+            results_diff = tf.sqrt(tf.reduce_mean(
                 tf.square(features1 - features2),
                 axis=1))
-            self._results_diff = tf.reshape(self._results_diff,[-1,1])
-        return self._results_diff
+            results_diff = tf.reshape(results_diff,[-1,1])
 
-    @property
-    def contrastive_loss(self, margin=1):
-        """Contrastive loss function for siamese networks"""
-        if self._contrastive_loss is None:
-            self._contrastive_loss = (
-                tf.reduce_mean((1-self.target_diff)*self.results_diff**2 / 2
-                + self.target_diff*(tf.maximum(0.,margin-self.results_diff))**2/2,axis=1))
-        return self._contrastive_loss
+            self._loss = (
+                tf.reduce_mean((1-target_diff)*results_diff**2 / 2
+                + target_diff*(tf.maximum(0.,self.margin-results_diff))**2/2,axis=1))
+        return self._loss
 
     @property
     def train_op(self):
@@ -59,7 +54,7 @@ class whalenet():
         if self._train_op is None:
             opt = tf.train.AdamOptimizer(
                 learning_rate = self.learning_rate)
-            self._train_op = opt.minimize(self.contrastive_loss,
+            self._train_op = opt.minimize(self.loss,
                                           global_step=self.global_step)
         return self._train_op
 
@@ -67,19 +62,22 @@ class whalenet():
     def summary_op(self):
         """Function to write the summary, returns property"""
         if self._summary_op is None:
-            tf.summary.scalar("loss", self.contrastive_loss)
-            tf.summary.histogram("histogram_loss", self.contrastive_loss)
+            tf.summary.scalar("loss", self.loss)
+            tf.summary.histogram("histogram_loss", self.loss)
             self._summary_op =  tf.summary.merge_all()
         return self._summary_op
 
 
-    def train(self, trainsteps=5000, printstep=500):
+    def train(self, train_steps=5000, print_step=500):
         """Trains model and saves to checkpoints
         arguments:
-            trainsteps: number of training steps (int)
-            printstep: output/summary after this number of steps (int)
+            train_steps: number of training steps (int)
+            print_step: output/summary after this number of steps (int)
         """
         _train_op = self.train_op               # build the model
+        print("building model")
+
+        init = tf.global_variables_initializer()
 
         try:
             os.mkdir('./checkpoints/%s' %self.model_name)
@@ -87,10 +85,10 @@ class whalenet():
             pass
 
         saver = tf.train.Saver()
-        l = np.zeros(trainsteps)
+        l = np.zeros(train_steps)
         with tf.Session() as sess:
-            sess.run(tf.global_variables_initializer())
             sess.run(self.iterator.initializer)
+            sess.run(init)
             ckpt = tf.train.get_checkpoint_state(os.path.dirname(
                 './checkpoints/%s/checkpoint' % self.model_name))
             # if that checkpoint exists, restore from checkpoint
@@ -98,24 +96,17 @@ class whalenet():
                 saver.restore(sess, ckpt.model_checkpoint_path)
                 print("Model restored, step = %d" % self.global_step.eval())
 
-            writer_train = tf.summary.FileWriter('./graphs/prediction/train/%s'
+            writer_train = tf.summary.FileWriter(
+                './graphs/prediction/train/%s'
                 %self.model_name, sess.graph)
             initial_step = self.global_step.eval()
 
             avg_loss = 0
-            next_batch = self.iterator.get_next()
-            for step in range(initial_step, initial_step+trainsteps):
-                self.img1, self.img2, self.target_diff = next_batch
-
-                # target, real, loss = sess.run([self.target_diff, self.results_diff, self.contrastive_loss])
-                # print(target)
-                # print(real)
-                # print(loss)
-
+            for step in range(initial_step, initial_step+train_steps):
                 _,loss = sess.run(
-                    [self.train_op,self.contrastive_loss])#,self.summary_op])
-                avg_loss = avg_loss+np.mean(loss)/printstep
-                if ((step+1)%printstep==0):
+                    [_train_op,self.loss])#,self.summary_op])
+                avg_loss = avg_loss+np.mean(loss)/print_step
+                if ((step+1)%print_step==0):
                     # writer_train.add_summary(summary, global_step=step)
                     print('Step {}: Train loss {:.3f}'.format(step, avg_loss))
                     avg_loss = 0
---
+++ src/whaleid/task.py
@@ -0,0 +1,77 @@
+import argparse
+import pandas as pd
+
+import model
+import datatools
+
+def train_model():
+    parser = argparse.ArgumentParser()
+
+    # Training arguments
+    parser.add_argument(
+        '--batch_size',
+        help = 'Batch size',
+        default = 32,
+        type = int
+    )
+    parser.add_argument(
+        '--train_steps',
+        help = 'Number of steps to train',
+        default = 5000,
+        type = int
+    )
+    parser.add_argument(
+        '--print_step',
+        help = 'output/summary after this number of steps',
+        default = 500,
+        type = int
+    )
+    parser.add_argument(
+        '--data_dir',
+        help = 'Directory with training data',
+        default = '../../data/train',
+        type = str
+    )
+    parser.add_argument(
+        '--labels_file',
+        help = 'csv file with image labels',
+        default = '../../data/labels_c',
+        type = str
+    )
+    parser.add_argument(
+        '--learning_rate',
+        help = 'learning rate',
+        default = 0.001,
+        type = float
+    )
+    parser.add_argument(
+        '--margin',
+        help = 'margin for contrastive loss',
+        default = 1,
+        type = int
+    )
+    args = parser.parse_args()
+
+    # Read labels and create data pipeline
+    labels = pd.read_csv(args.labels_file)
+    pipeline = datatools.datapipeline(
+        datadir = args.data_dir,
+        labels = labels,
+        batchsize = args.batch_size)
+    dataset = pipeline.build()
+    iter = dataset.make_initializable_iterator()
+
+    # Create model and train
+    net = model.whalenet(
+        iter,
+        learning_rate = args.learning_rate,
+        margin = args.margin,
+        model_name = "whale1")
+
+    net.train(
+        train_steps = args.train_steps,
+        print_step = args.print_step)
+
+
+if __name__ == '__main__':
+    train_model()
